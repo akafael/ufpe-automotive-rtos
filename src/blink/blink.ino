34978f4eb1205df3c8ca6beaad545814b6ae6591
Rafael <akafael@users.noreply.github.com>
2023-05-16 22:38:06 -0300

    Add blink example using trampoline

--- src/blink/blink.ino
+++ src/blink/blink.ino
@@ -6,20 +6,18 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include "Board.h"
 // Executa somente uma vez após pressionar o botão reset ou ligar
+
+int stateLed = HIGH;
+
 void setup() {
-  // Inicializa o pino digital como entrada.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 //Executa continuamente
 void loop() {
-  //Liga o led
-  digitalWrite (LED_BUILTIN, HIGH);
-  //Aguarda 200ms
-  delay(200);
-  //Desliga o led
-  digitalWrite (LED_BUILTIN, LOW);
-  //Aguarda 200ms
-  delay(200);
+  stateLed = !stateLed;
+
+  digitalWrite (LED_BUILTIN, stateLed);
+  delay(150);
 }
 
