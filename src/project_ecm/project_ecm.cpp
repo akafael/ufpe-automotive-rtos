8b1e5c3a922798d51984d508f8066571c173bd4c
Rafael <akafael@users.noreply.github.com>
2023-06-07 23:15:18 -0300

    Add can read to project_ecm

--- src/project_ecm/project_ecm.cpp
+++ src/project_ecm/project_ecm.cpp
@@ -4,8 +4,13 @@
 
 #include "Arduino.h"
 #include "tpl_os.h"
+#include <SPI.h>
+#include <mcp_can.h>
 
 #include "project_ecm.hpp"
+
+#define CAN_SPI_INT 2 // Set INT to pin 2
+MCP_CAN CAN_SPI(10);  // Set CS to pin 10
 
 uint8_t currentGear = 0;
 uint16_t currentRPM = 0;
@@ -14,6 +19,17 @@ float currentVelocity = 0;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
+
+  // Start CAN Device
+  if (CAN_SPI.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK)
+    Serial.println("MCP2515 Initialized Successfully!");
+  else
+    Serial.println("Error Initializing MCP2515...");
+
+  // CAN Settings
+  pinMode(CAN_SPI_INT, INPUT); // Configuring pin for /INT input
+  CAN_SPI.setMode(MCP_NORMAL); // Set operation mode to normal so the MCP2515
+                               // sends acks to received data.
 }
 
 /**
@@ -33,10 +49,21 @@ TASK(periodicTaskRead) {
 }
 
 /**
- * REad msg with Gear value
+ * Read msg with Gear value
  */
 TASK(periodicTaskReadGearMsg) {
-  currentGear = 1;
+  // If CAN_SPI_INT pin is low, read receive buffer
+  if (!digitalRead(CAN_SPI_INT)) {
+    uint32_t rxId;
+    uint8_t dataLenght = 0;
+    uint8_t rxBuffer[8];
+
+    // Read Data from CAN
+    CAN_SPI.readMsgBuf(&rxId, &dataLenght, rxBuffer);
+
+    // const uint32_t msgId = (rxId & 0x1FFFFFFF);
+    currentGear = rxBuffer[4];
+  }
 
   Serial.print("Gear: ");
   Serial.println(currentGear);
