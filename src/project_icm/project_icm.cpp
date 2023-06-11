b5e54836d717352258e2e86e50c0a1dda4ad1b56
Rafael <akafael@users.noreply.github.com>
2023-06-11 16:44:55 -0300

    Add decode and encode draft functions for gear msg

--- src/project_icm/project_icm.cpp
+++ src/project_icm/project_icm.cpp
@@ -7,7 +7,8 @@
 #include <SPI.h>
 #include <mcp_can.h>
 
-#include "project_ecm.hpp"
+#include "../project_lib/Board.h"
+#include "../project_lib/Utils.hpp"
 
 #define CAN_SPI_INT 2 // Set INT to pin 2
 MCP_CAN CAN_SPI(10);  // Set CS to pin 10
@@ -32,75 +33,40 @@ void setup() {
                                // sends acks to received data.
 }
 
-
-/**
- * Read msg with Gear value
- */
- 
-TASK(periodicTaskReadGearMsg) {
+TASK(periodicTaskReadCANMsg) {
   // If CAN_SPI_INT pin is low, read receive buffer
   if (!digitalRead(CAN_SPI_INT)) {
-    uint32_t rxId;
-    uint8_t dataLenght = 0;
-    uint8_t rxBuffer[8];
+    uint32_t rxId;          // CAN Frame ID
+    uint8_t dataLenght = 0; // Payload data length
+    uint8_t rxBuffer[8];    // Payload data
 
     // Read Data from CAN
     CAN_SPI.readMsgBuf(&rxId, &dataLenght, rxBuffer);
 
-    // const uint32_t msgId = (rxId & 0x1FFFFFFF);
-    if (rxId == IdMsgGear) {
-      currentGear = eval_gear(rxBuffer[0]);
+    // Decode msg payload considering msgId
+    const uint32_t msgId = (rxId & 0x1FFFFFFF);
+    if (msgId == IdMsgGear) {
+      const uint8_t gear = decode_gearData(rxBuffer[0]);
+      currentGear = eval_gear(gear);
+    } else if (msgId == IdMsgRPM) {
+      // currentRPM = eval_rpm(rxBuffer[0]);
+    } else if (msgId == IdMsgVelocity) {
+      // currentVelocity = calc_velocity(rxBuffer[0]);
     }
   }
 
+  TerminateTask();
+}
+
+TASK(periodicTaskPrint) {
   Serial.print("Gear: ");
   Serial.println(currentGear);
 
-  TerminateTask();
-}
-
-TASK(periodicTaskReadRPMMsg) {
-  // If CAN_SPI_INT pin is low, read receive buffer
-  if (!digitalRead(CAN_SPI_INT)) {
-    uint32_t rxId;
-    uint16_t dataLenght = 0;
-    uint16_t rxBuffer[8];
-
-    // Read Data from CAN
-    CAN_SPI.readMsgBuf(&rxId, &dataLenght, rxBuffer);
-
-    // const uint32_t msgId = (rxId & 0x1FFFFFFF);
-    if (rxId == IdMsgRPM) {
-      currentRPM = eval_rpm(rxBuffer[0]);
-    }
-  }
-
   Serial.print("RPM: ");
   Serial.println(currentRPM);
-
-  TerminateTask();
-}
-
-TASK(periodicTaskReadVelMsg) {
-  // If CAN_SPI_INT pin is low, read receive buffer
-  if (!digitalRead(CAN_SPI_INT)) {
-    uint32_t rxId;
-    uint16_t dataLenght = 0;
-    uint16_t rxBuffer[8];
-
-    // Read Data from CAN
-    CAN_SPI.readMsgBuf(&rxId, &dataLenght, rxBuffer);
-
-    // const uint32_t msgId = (rxId & 0x1FFFFFFF);
-    if (rxId == IdMsgVelocity) {
-      currentVelocity = calc_velocity(rxBuffer[0]);
-    }
-  }
 
   Serial.print("Velocity: ");
   Serial.println(currentVelocity);
 
   TerminateTask();
 }
-
-
